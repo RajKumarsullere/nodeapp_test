d4fc2cdb6f02b167dcdf48c8f2cc695e3096d2c2
rajsullere <100362493+rajsullere@users.noreply.github.com>
2022-09-22 13:48:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
-});
-
+const express = require('express');
+const app = express();
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
-});
-app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
-});
+res.send('{ "response": "Hello World" }');
+    {
 app.listen(process.env.PORT || 3000);
-module.exports = app;
+module.exports = app;};
